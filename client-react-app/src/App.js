b0c745daff9dd20483042b4c9cbc9ad52252f6d4
mohammed <mohammedacademy4@gmail.com>
2021-07-12 12:07:31 +0100

    add react-router-dom

--- client-react-app/src/App.js
+++ client-react-app/src/App.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Wallet from './components/Wallet';
-import Header from './components/Header';
+import Wallet from "./components/Wallet";
+import Header from "./components/Header";
 
 import "./App.css";
+
+// !hey this code is not work
+// ? is this still working
+// TODO this function is not finished
 
 function App() {
   return (
     <div className="container">
       <Header />
       <div className="connect">
-        <h3> Welcome to the electronic health record (EHRethChain) DApp powered by Ethereum Blockchain. </h3>
-        <Wallet button_name = "Connect"/>
+        <h3>
+          {" "}
+          Welcome to the electronic health record (EHRethChain) DApp powered by
+          Ethereum Blockchain.{" "}
+        </h3>
+        <Wallet button_name="Connect" />
       </div>
     </div>
-    );
+  );
 }
-
 
 export default App;
